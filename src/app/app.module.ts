cea7cd8845fceabafd73bbd7132203741eb63282
Vishnu <vishnunroshan@bionworks.com>
2018-11-27 12:35:15 +0530

    routing basic setup
    just basic routing functionality added. plans to extend the functionality further

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+import { MarketComponent } from './market/market.component';
+import { ToolsComponent } from './tools/tools.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'news',
+    component: NewsComponent
+  },
+  {
+    path: 'market',
+    component: MarketComponent
+  },
+  {
+    path: 'tools',
+    component: ToolsComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    MarketComponent,
+    ToolsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
